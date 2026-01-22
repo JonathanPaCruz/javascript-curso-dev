a92ae3d8b04c1faf06615f3d441799c76201decb
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-01-21 20:44:37 -0600

    Se añade ejemplos para palabras reservadas en js

---
+++ Fundamentos/assets/js/palabras-reservadas.js
@@ -0,0 +1,32 @@
+console.log('Hola mundo!');
+let constante = 123;
+// const
+// if
+// try
+// while
+// for()
+
+// Estándares para nombres de variables:
+// No se pueden crear nombres de variables que comiencen con números:
+// let 1abc = 123;
+// Se puede iniciar con _variable;
+let _varTest = 'variable de test';
+// Se puede iniciar con $variable;
+let $varTest = 'variable de test';
+
+// Se pueden crear variables con números:
+let varTest123 = '123';
+// No se pueden hacer variables con "." :
+// let varTest.123 = '123';
+// El punto significa que se quiere acceder a alguna propiedad o método propio del objeto.
+
+// Las variables deben tener un significado semántico.
+// let x = 'Jonathan'; <- esto es incorrecto
+let nombreUsuario = 'Jonathan'; // Esto es lo correcto.
+
+// También existe el upperCamelCase, esto se ve en nombres de clases : class
+class JuegoMes {
+
+};
+
+
